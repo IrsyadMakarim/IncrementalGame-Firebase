0f64e9252a58b82df6b6a265574b34e4b920db47
irsyad0141 <1rsy4d.m@gmail.com>
2021-11-19 12:37:33 +0700

    Finished Step 2 - 4

--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -17,7 +17,7 @@ public class ResourceController : MonoBehaviour
         set
         {
             UserDataManager.Progress.ResourcesLevels[_index] = value;
-            UserDataManager.Save();
+            UserDataManager.Save(true);
         }
 
         get
@@ -89,6 +89,7 @@ public class ResourceController : MonoBehaviour
 
         ResourceUpgradeCost.text = $"Upgrade Cost\n{ GetUpgradeCost () }";
         ResourceDescription.text = $"{ _config.Name } Lv. { _level }\n+{ GetOutput ().ToString ("0") }";
+        AnalyticsManager.LogUpgradeEvent(_index, _level);
     }
 
     public void UnlockResource ()
@@ -102,6 +103,7 @@ public class ResourceController : MonoBehaviour
         SetUnlocked (true);
         GameManager.Instance.ShowNextResource ();
         AchievementController.Instance.UnlockAchievement (AchievementType.UnlockResource, _config.Name);
+        AnalyticsManager.LogUnlockEvent(_index);
     }
 
     public void SetUnlocked (bool unlocked)
@@ -112,7 +114,7 @@ public class ResourceController : MonoBehaviour
             if (!UserDataManager.HasResources(_index))
             {
                 UserDataManager.Progress.ResourcesLevels.Add(_level);
-                UserDataManager.Save();
+                UserDataManager.Save(true);
             }
         }
         ResourceImage.color = IsUnlocked ? Color.white : Color.grey;
